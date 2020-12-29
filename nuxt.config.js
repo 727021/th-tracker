1ee516fffe75789d0f61a5576a7d3f2896f86273
727021 <1834544+727021@users.noreply.github.com>
2020-12-29 12:54:57 -0700

    Add port to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    port: process.env.PORT || 3000,
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - todo-tracker',
